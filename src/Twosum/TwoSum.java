4fff0ec753be9b0a8217fc8f544c9b3fdaa26926
inspirationLG <1185379947@qq.com>
2020-01-09 17:11:56 +0800

    两数之和和猜数字

---
+++ src/Twosum/TwoSum.java
@@ -0,0 +1,20 @@
+package Twosum;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int dif = target - nums[i];
+            // j = i + 1 的目的是减少重复计算和避免两个元素下标相同
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == dif){
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
+
