d36c963942030df56a511429a2ea2ccd204de7e5
inspirationLG <1185379947@qq.com>
2020-01-13 17:23:29 +0800

    反转链表

--- src/reverseList/Solution.java
+++ src/reverseList/Solution.java
@@ -1,4 +1,31 @@
 package reverseList;
 
+import java.util.List;
+
+// * Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
 public class Solution {
+    public ListNode reverseList(ListNode head) {
+        //申请节点，pre和 cur，pre指向null
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while (cur != null) {
+            //记录当前节点
+            tmp = cur.next;
+            //然后将当前节点指向pre
+            cur.next = pre;
+            //pre和cur节点都前进一位
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
 }
